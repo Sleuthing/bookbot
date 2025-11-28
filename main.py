2bf625d076e7fe89d0afe253ad621340a94ce686
Sleuthing <anaslaykah@yahoo.com>
2025-11-28 17:44:34 +0100

    Added Report Code

--- main.py
+++ main.py
@@ -2,15 +2,19 @@ from stats import count_words, count_each_character
 
 def get_book_text(filepath):
     with open(filepath) as file:
+        print(f"Analyzing book found at {filepath}...")
         file_content = file.read()
     return file_content
 
 def main():
+    print("============ BOOKBOT ============")
     # print(get_book_text("./books/frankenstein.txt"))
     file_content = get_book_text("./books/frankenstein.txt")
     num_words = count_words(file_content)
     print(f"Found {num_words} total words")
-    print(count_each_character(file_content))
+    for k,v in count_each_character(file_content).items():
+        print(f"{k}: {v}")
+    print("============= END ===============")
 
 main()
 # if __name__ == 'main':
--- stats.py
+++ stats.py
@@ -1,11 +1,27 @@
 def count_words(string):
+    print("----------- Word Count ----------")
     word_count = list(filter(None,string.replace('\n',' ').split(' ')))
     return len(word_count)
     # return len(string.split(' '))
 
+def sort_on(items):
+    return items["num"]
+
 def count_each_character(string):
+    print("--------- Character Count -------")
     string = string.lower()
     output_dict = {}
     for character in set(list(string)):
-        output_dict[character] = string.count(character)
+        if character.isalpha():
+            output_dict[character] = string.count(character)
+    output_dict = dict(sorted(output_dict.items(),key=lambda item: item[1],reverse=True))
     return output_dict
+
+def get_sorted_list_of_dict(dict):
+    output_list_of_dict = []
+    for k,v in dict.items():
+        if k.isalpha():
+            output_list_of_dict.append({'char':k,'num':v})
+    output_list_of_dict.sort(reverse=True,key=lambda items: items["num"])
+    return output_list_of_dict
+
